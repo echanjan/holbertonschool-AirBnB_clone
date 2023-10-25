5dbacf5c74075d1aecd63fb918a5b25117d0cada
echanjan <erickchanjan300696@gmail.com>
2023-10-25 05:27:04 -0700

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/rin/python3
 """
-Este móduImportamos los módulos 'uuid' y 'datetime' para generar identificadores únicos
+Importamos los módulos 'uuid' y 'datetime' para generar identificadores únicos
 y para trabajar con fechas y horas.
 """
 
@@ -10,19 +10,22 @@ import datetime
 """
 Definimos una clase llamada BaseModel
 """
+
+
 class BaseModel:
 
     """
-    Metodo de inicialización que se ejecuta cuando se crea una nueva instancia de la clase.
+    Metodo de inicialización que se ejecuta cuando se crea una
+    nueva instancia de la clase.
     """
 
     def __init__(self):
         """
-        Asignamos un identificador único generado por 'uuid.uuid4()' y lo convertimos a una cadena.
-            self.id = str(uuid.uuid4())
-        
-        Asignamos la fecha y hora actuales al momento de creación de la instancia.
-            self.created_at = datetime.datetime.now()
+        Asignamos un identificador único generado por 'uuid.uuid4()'
+        y lo convertimos a una cadena.
+
+        Asignamos la fecha y hora actuales al momento de creación
+        de la instancia.
 
         Asignamos la fecha y hora actuales al atributo 'updated_at' también
         para que esté igual al momento de creación al principio.
@@ -33,25 +36,29 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     """
-    Método especial que define cómo se imprimirá una instancia de la clase cuando se use la función 'print()'.
+    Método especial que define cómo se imprimirá una instancia de la clase
+    cuando se use la función 'print()'.
     """
     def __str__(self):
         return f"[BaseModel] ({self.id}) {self.__dict__}"
-    
+
     """
-    Método 'save' que actualiza el atributo 'updated_at' con la fecha y hora actuales.
+    Método 'save' que actualiza el atributo 'updated_at'
+    con la fecha y hora actuales.
     """
     def save(self):
         self.updated_at = datetime.datetime.now()
 
     """
-    Método 'to_dict' que retorna un diccionario con los atributos de la instancia.
+    Método 'to_dict' que retorna un diccionario con los atributos
+    de la instancia.
     """
     def to_dict(self):
         """
         Copiamos el diccionario de atributos usando 'self.__dict__.copy()'
 
-        Añadimos una clave '__class__' al diccionario que contiene el nombre de la clase.
+        Añadimos una clave '__class__' al diccionario que contiene
+        el nombre de la clase.
 
         Convertimos las fechas y horas a una cadena en formato ISO.
 
