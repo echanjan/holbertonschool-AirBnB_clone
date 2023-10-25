7fa47d5eb6ee1b4f13d4e66fac190cd19ecbc9b8
echanjan <erickchanjan300696@gmail.com>
2023-10-24 20:52:50 -0700

    update models/base_model.py, models/__init__.py, tests/

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,8 @@ class BaseModel:
         """
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = self.created_at
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
@@ -30,13 +30,13 @@ class BaseModel:
         Return:
             str: Representacion de instancia.
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
         Este método actualiza la fecha de creación updated_at.
         """
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
