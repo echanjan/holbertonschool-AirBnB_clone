67db0c3606c982e761959f9b81645e6f68c72148
echanjan <erickchanjan300696@gmail.com>
2023-10-26 17:54:31 -0700

    update models/base_model.py, models/__init__.py, tests/

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_save_method_updates_updated_at(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        new_updated_at = self.base_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_method(self):
+        expected_dict = {
+            'id': self.base_model.id,
+            'created_at': self.base_model.created_at.isoformat(),
+            'updated_at': self.base_model.updated_at.isoformat(),
+            '__class__': 'BaseModel'
+        }
+        self.assertEqual(self.base_model.to_dict(), expected_dict)
+
+    def test_id_attribute(self):
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_created_at_attribute(self):
+        self.assertIsInstance(self.base_model.created_at, datetime.datetime)
+
+    def test_str_method(self):
+        expected_output = f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}"
+        self.assertEqual(str(self.base_model), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
+
