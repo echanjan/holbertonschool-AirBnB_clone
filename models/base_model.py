1f788758a40a042292e13d6eff1c201c7b818416
echanjan <erickchanjan300696@gmail.com>
2023-10-26 20:02:37 -0700

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,23 @@
 #!/usr/rin/python3
 """
-Importamos los módulos 'uuid' y 'datetime' para generar identificadores únicos
-y para trabajar con fechas y horas.
+Este módulo define la clase BaseModel
 """
 
 import uuid
 import datetime
 
-"""
-Definimos una clase llamada BaseModel
-"""
-
 
 class BaseModel:
-
     """
-    Metodo de inicialización que se ejecuta cuando se crea una
-    nueva instancia de la clase.
+    Esta es la clase que heredarán las demas clases
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Asignamos un identificador único generado por 'uuid.uuid4()'
-        y lo convertimos a una cadena.
-
-        Asignamos la fecha y hora actuales al momento de creación
-        de la instancia.
-
-        Asignamos la fecha y hora actuales al atributo 'updated_at' también
-        para que esté igual al momento de creación al principio.
+        Este método inicializa los atributos:
+            id, created_at, updated_at
+        Args:
+            **kwargs: recibe un diccionario.
         """
 
         if kwargs:
@@ -36,45 +25,36 @@ class BaseModel:
                 if k != "__class__":
                     setattr(self, k, v)
             self.__dict__["created_at"] = datetime.strptime(
-                self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
             self.__dict__["updated_at"] = datetime.strptime(
-                self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    """
-    Método especial que define cómo se imprimirá una instancia de la clase
-    cuando se use la función 'print()'.
-    """
     def __str__(self):
-        return f"[BaseModel] ({self.id}) {self.__dict__}"
+        """
+        Este metodo retorna una representación de nuestra instancia
+        """
 
-    """
-    Método 'save' que actualiza el atributo 'updated_at'
-    con la fecha y hora actuales.
-    """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
     def save(self):
+        """
+        Este método actualiza la fecha de creación updated_at.
+        """
+
         self.updated_at = datetime.datetime.now()
 
-    """
-    Método 'to_dict' que retorna un diccionario con los atributos
-    de la instancia.
-    """
     def to_dict(self):
         """
-        Copiamos el diccionario de atributos usando 'self.__dict__.copy()'
-
-        Añadimos una clave '__class__' al diccionario que contiene
-        el nombre de la clase.
-
-        Convertimos las fechas y horas a una cadena en formato ISO.
-
-        Retornamos el diccionario modificado.
+        Este método retorna un diccionario con los atributos de instancia.
         """
+
         attributes = self.__dict__.copy()
         attributes['__class__'] = self.__class__.__name__
         attributes['created_at'] = self.created_at.isoformat()
