302d9cefd6371f42674eb6881c50a7dbcc94c778
echanjan <erickchanjan300696@gmail.com>
2023-11-05 19:09:47 -0800

    update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Este modulo define la clase de FileStorage
+Este módulo define la clase FileStorage
 """
-
 import json
 from os import path
+import datetime
 
 
 class FileStorage:
     """
-    La clase FileStorahe se encargará de serializar y
+    La clase FileStorage se encargará de serializar y
     deserealizar archivos para recuperar instancias de BaseModel
     """
     __file_path = "file.json"
@@ -17,7 +17,7 @@ class FileStorage:
 
     def all(self):
         """
-        Retoma el contenido de .__objects.
+        Retorna el contenido de .__objects.
         Returns:
             dict
         """
@@ -28,7 +28,7 @@ class FileStorage:
         Este método almacena las instancias de clase,
         recibidas en .__objects.
         Args:
-            obj (objecs): Instancias a almacenar.
+            obj (objec): Instancias a almacenar.
         """
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
@@ -43,7 +43,7 @@ class FileStorage:
         new_dict = {}
 
         for k, obj in FileStorage.__objects.items():
-            for k, obj in FileStorage.__objects.items():
+            new_dict[k] = obj.to_dict()
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(new_dict, f)
 
@@ -52,7 +52,7 @@ class FileStorage:
         Este método lee un archivo en formato .json, que se guardo
         previamente con el método .save(), el diccionario recuperado
         se convertirá a objetos de python (dict) que seran utilizados
-        para recuprar las instancias de clase BaseModel creadas
+        para recuperar las instancias de clase BaseModel creadas
         anteriormente, estas instancias serán almacenadas en .__objects.
         """
         if path.exists(FileStorage.__file_path):
@@ -62,3 +62,20 @@ class FileStorage:
                 from models.base_model import BaseModel
                 bs = BaseModel(**v)
                 FileStorage.__objects[k] = bs
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+                "BaseModel":
+                    {"id": str,
+                    "created_at": datetime.datetime,
+                    "updated_at": datetime.datetime},
+                    "User":
+                {"email": str,
+                    "password": str,
+                    "first_name": str,
+                    "last_name": str
+                    },
+        }
+
+        return attributes
