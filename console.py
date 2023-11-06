db7096cb9ae84724faa378ca42720073f089ab15
echanjan <erickchanjan300696@gmail.com>
2023-11-05 19:32:36 -0800

    update

--- console.py
+++ console.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-
+"""
+Este módulo expone la clase
+HBNBCommand(cmd.Cmd)
+"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Esta clase hereda de cmd.Cmd, desde aqui se gestionará
+    nuestro proyecto.
+    """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-    """Quit command to exit the program\n"""
-        return True
-
-    def do_EOF(self, arg):
-    """Quit command to exit the program\n"""
-        return True
-
-    def emptyline(self):
-        pass
-
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
