f58d29afd23a05e729fa2f69f9b420e744dc57a3
echanjan <erickchanjan300696@gmail.com>
2023-11-05 17:04:59 -0800

    update amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define la clase Amenity"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Muestras las comodidades de la habitacion
+
+    Atributos:
+    name (str) = nombres de las comodidades
+    """
+
+    name = ""
