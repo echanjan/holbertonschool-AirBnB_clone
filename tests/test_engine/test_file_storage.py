72bb4b019f919c8c69091870450a5a9ec93fd546
echanjan <erickchanjan300696@gmail.com>
2023-11-05 17:16:59 -0800

    update tests

--- tests/test_engine/test_file_storage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import unittest
-
-from models.engine.file_storage import FileStorage
-
-
-class TestStorage(unittest.TestCase):
-    def test_file_path(self):
-        self.assertIsNone(FileStorage.__file_path)
