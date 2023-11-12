05ef3cdcc88df5fe58b068a4c1c5ed8c60b04068
echanjan <erickchanjan300696@gmail.com>
2023-11-12 16:54:05 -0500

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/rin/python3
+#!/usr/bin/python3
 """
 Este módulo define la clase BaseModel
 """
@@ -10,29 +10,29 @@ from models import storage
 
 class BaseModel:
     """
-    Esta clase expone BaseModel, la que será nuestra superclase.
+    Este clase expone BaseModel, la que será nuestra superclase.
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Este método inicializa los sgte:
+        Este método inicializa lo sgte:
             id, created_at, updated_at
         Attr:
-            id (str): Genera un id cada vez que se instancia
+            id (str): Genera un id cada vez que se instancia.
             created_ad (str): Genera la hora y fecha cada vez
-            que se instancia un nuevo objeto.
-            updated_at (str): Genera y actualiza la hora y fecha
-            cada vez que se cambia nuestra instancia.
+                    que se instancia un nuevo objeto.
+            updated_at (str): Genera y actualiza la hora y fecha,
+                    cada vez que se cambia nuestra instancia.
         """
         if bool(kwargs):
             for k, v in kwargs.items():
-                if k != "__class__":
+                if k != '__class__':
                     setattr(self, k, v)
 
             self.__dict__["created_at"] = datetime.strptime(
-                    self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
             self.__dict__["updated_at"] = datetime.strptime(
-                    self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -40,15 +40,15 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Retorna una cadena de este formado:
-        BaseModel (uuid4 type id) self.__dict__
+        """Returns a string in this format:
+            BaseModel (uuid4 type id) self.__dict__
         """
         return f"[{self.__class__.__name__}] ({self.id}) "\
             + str({k: v for k, v in self.__dict__.items() if k != '__class__'})
 
     def save(self):
         """Este método actualiza .updated_at"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
@@ -57,7 +57,6 @@ class BaseModel:
         Returns:
             new_dict (dict)
         """
-
         new_dict = self.__dict__.copy()
         new_dict['created_at'] = new_dict['created_at'].isoformat()
         new_dict['updated_at'] = new_dict['updated_at'].isoformat()
