9b11691e21202b67048d84bf92dc055c36e76616
echanjan <erickchanjan300696@gmail.com>
2023-11-12 17:20:04 -0500

    update tests

--- tests/test_place.py
+++ tests/test_place.py
@@ -2,7 +2,7 @@
 import unittest
 
 from models.place import Place
-
+from models.base_model import BaseModel
 
 class TestCity(unittest.TestCase):
 
@@ -22,3 +22,29 @@ class TestCity(unittest.TestCase):
         self.assertNotIn("latitude", place_attr)
         self.assertNotIn("longitude", place_attr)
         self.assertNotIn("amenity_ids", place_attr)
+
+    def test_is_instantiated(self):
+        self.assertIsNotNone(self.place)
+
+    def test_inheric(self):
+        self.assertIsInstance(self.place, BaseModel)
+
+    def test_str_magic(self):
+        name_class = self.place.__class__.__name__
+        id_obj = self.place.id
+        dict_obj = self.place.__dict__
+        str_magic = f"[{name_class}] ({id_obj}) {dict_obj}"
+
+        self.assertEqual(str(self.place), str_magic)
+
+    def test_exist_attr(self):
+        city_id = "my_id"
+        name = "name"
+        self.place.city_id = city_id
+        self.place.name = name
+        self.assertTrue(self.place.city_id)
+        self.assertTrue(self.place.name)
+
+    def test_type_attr(self):
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.name, str)
